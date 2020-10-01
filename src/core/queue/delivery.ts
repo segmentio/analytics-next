f82028c6d7af29dc928bd1c8ae329d079f865561
Netto Farah <nettofarah@gmail.com>
2020-10-01 18:27:27 -0300

    Wrap extensions in a promise so we can await on them

--- src/core/queue/delivery.ts
+++ src/core/queue/delivery.ts
@@ -15,7 +15,7 @@ export async function attempt(ctx: Context, extension: Extension): Promise<Conte
     return ctx
   }
 
-  const newCtx = await hook(ctx)
+  const newCtx = await Promise.resolve(hook(ctx))
     .then((ctx) => {
       const done = new Date().getTime() - start
       ctx.stats.gauge('extension_time', done, [`extension:${extension.name}`])
