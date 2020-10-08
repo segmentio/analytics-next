5fb2b5006ba8bcbf09d483a25f06b42178fa9d79
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2020-10-08 13:18:55 -0400

    Add fields to SegmentEvent that get added from the segmentio integration (#20)

--- src/core/events/index.ts
+++ src/core/events/index.ts
@@ -1,6 +1,7 @@
 import uuid from '@lukeed/uuid'
 import { ID, User } from '../user'
 import { CompactMetric } from '../stats'
+
 export type Integrations = Record<string, boolean>
 
 export type Options = {
@@ -9,9 +10,77 @@ export type Options = {
 }
 
 interface AnalyticsContext {
-  page?: object
+  page?: {
+    /**
+     *  {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L151}
+     */
+    path?: string
+    referrer?: string
+    search?: string
+    title?: string
+    url?: string
+  }
   metrics?: CompactMetric[]
+
   [key: string]: any
+
+  /**
+   *  {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L285}
+   */
+  userAgent?: string
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L286-L289}
+   */
+  locale?: string
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L290-L291}
+   */
+  library?: {
+    name: 'ajs-next'
+    version: string
+  }
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L292-L301}
+   */
+  traits?: {
+    crossDomainId: string
+  }
+
+  /**
+   * utm params
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L303-L305}
+   * {@link https://github.com/segmentio/utm-params/blob/master/lib/index.js#L49}
+   */
+  campaign?: {
+    /**
+     * This can also come from the "utm_campaign" param
+     *
+     * {@link https://github.com/segmentio/utm-params/blob/master/lib/index.js#L40}
+     */
+    name: string
+    term: string
+    source: string
+    medium: string
+    content: string
+  }
+
+  /**
+   *  {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L415}
+   */
+  referrer?: {
+    btid?: string
+    urid?: string
+  }
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L322}
+   */
+  amp?: {
+    id: string
+  }
 }
 
 export interface SegmentEvent {
@@ -31,6 +100,25 @@ export interface SegmentEvent {
   anonymousId?: ID
 
   event?: string
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L284}
+   */
+  writeKey?: string
+
+  /**
+   *  {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L151}
+   */
+  sentAt?: Date
+
+  /**
+   * {@link https://github.com/segmentio/analytics.js-integrations/blob/2d5c637c022d2661c23449aed237d0d546bf062d/integrations/segmentio/lib/index.js#L311-L320}
+   */
+  _metadata?: {
+    failedInitializations?: unknown[]
+    bundled: string[]
+    unbundledIntegrations: string[]
+  }
 }
 
 export class EventFactory {
