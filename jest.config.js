b96d3be82a280885d43fb1091ba84c7486565217
Netto Farah <nettofarah@gmail.com>
2020-10-28 19:43:40 -0300

    Remove global teardown

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,4 @@ module.exports = {
       isolatedModules: true,
     },
   },
-  globalTeardown: './jest.teardown.js',
 }
--- jest.teardown.js
+++
@@ -1,3 +0,0 @@
-module.exports = async function () {
-  process.exit(0)
-}
