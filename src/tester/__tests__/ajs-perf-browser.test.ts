294c972e0f573f1ff9dc2cf53d80068e48a6b741
Netto Farah <nettofarah@gmail.com>
2020-10-28 12:09:16 -0300

    Inline browser tester

--- src/tester/__tests__/ajs-perf-browser.test.ts
+++ src/tester/__tests__/ajs-perf-browser.test.ts
@@ -41,4 +41,20 @@ describe('Performance', () => {
 
     await analyticsStub.browserPage.close()
   })
+
+  it('loads ajs in a browser', async () => {
+    jest.setTimeout(10000)
+    const analyticsStub = await tester('***REMOVED***', 'http://localhost:3001', 'chromium', true)
+
+    const ctx = await analyticsStub.track('hi', {
+      test: 'prop',
+    })
+
+    expect(ctx.event.event).toEqual('hi')
+    expect(ctx.event.properties).toEqual({
+      test: 'prop',
+    })
+
+    await analyticsStub.browserPage.close()
+  })
 })
