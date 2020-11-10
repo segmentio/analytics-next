0fba200fc394d1d5992a2c5038304e8c96b96837
Netto Farah <nettofarah+github@gmail.com>
2020-11-10 15:25:09 -0300

    Create uniq CI script (#55)
    * Create uniq CI script
    
    * Expire cache
    
    * Do not freeze lockfile
    
    * Use new facade
    
    * Log in to NPM from within buildkite
    
    * Fix typo
    
    * Re-install playwright

---
+++ scripts/ci.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo '--- Install development dependencies'
+yarn install
+yarn add --dev playwright
+
+echo '--- Build bundles'
+NODE_ENV=production yarn build
+
+echo '--- Check Size'
+yarn size-limit
+
+echo '--- Lint files'
+yarn lint
+
+echo '--- Run tests'
+yarn test
