b8acbb59f78af2886dd4718c08b7f51a2cf94c5f
dsjackins <daniel.jackins@segment.com>
2021-01-13 13:09:25 -0800

    add bonobos (#96)

--- e2e-tests/recorder/config.ts
+++ e2e-tests/recorder/config.ts
@@ -4,6 +4,7 @@ import staples from '../cases/staples'
 import local from '../cases/local'
 import ritual from '../cases/ritual'
 import classpass from '../cases/classpass'
+import bonobos from '../cases/bonobos'
 
 export const DEVTOOLS = process.env.DEVTOOLS === 'true'
 export const AJS_VERSION = process.env.AJS_VERSION || 'next'
@@ -17,7 +18,7 @@ export const TRACKING_API_URLS = [
 ]
 
 const CASES = process.env.CASES
-const allScenarios = [segment, milanuncios, staples, local, ritual, classpass]
+const allScenarios = [segment, milanuncios, staples, local, ritual, classpass, bonobos]
 
 export const cases = allScenarios.filter(
   (scenario) => CASES?.split(',').includes(scenario.name) ?? true
