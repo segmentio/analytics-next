6da8e08863c3553fa5ef2ac4ac0ab8bcd5a25d7b
Netto Farah <nettofarah+github@gmail.com>
2021-05-03 17:17:53 -0500

    Detect CSP Errors (#189)
    * Detect CSP Errors
    
    Detect CSP errors when a website hasn't configured the Segment CDN or
    their own CDN to serve ajs settings.
    
    This detection logic will help customers rollout AJS 2.0 on their
    websites without the risk on incurring data loss.
    
    * Simpler detection
    
    * Use analytics classic path
    
    * Add tests

---
+++ src/__tests__/standalone.test.ts
@@ -0,0 +1,118 @@
+import jsdom, { JSDOM } from 'jsdom'
+import { LegacySettings } from '../browser'
+import { snippet } from '../tester/__fixtures__/segment-snippet'
+import { pWhile } from '../lib/p-while'
+import { mocked } from 'ts-jest/utils'
+import unfetch from 'unfetch'
+
+const cdnResponse: LegacySettings = {
+  integrations: {
+    Zapier: {
+      type: 'server',
+    },
+    'Amazon S3': {},
+    Amplitude: {
+      type: 'browser',
+    },
+    Segmentio: {
+      type: 'browser',
+    },
+    Iterable: {
+      type: 'browser',
+      name: 'Iterable',
+    },
+  },
+}
+
+const fetchSettings = Promise.resolve({
+  json: () => Promise.resolve(cdnResponse),
+})
+
+jest.mock('unfetch', () => {
+  return jest.fn()
+})
+
+describe('standalone bundle', () => {
+  const segmentDotCom = `***REMOVED***`
+
+  let jsd: JSDOM
+
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.resetAllMocks()
+
+    jest.spyOn(console, 'warn').mockImplementationOnce(() => {})
+
+    // @ts-ignore ignore Response required fields
+    mocked(unfetch).mockImplementation((): Promise<Response> => fetchSettings)
+
+    const html = `
+    <!DOCTYPE html>
+      <head>
+        <script>
+          ${snippet(segmentDotCom, true)}
+        </script>
+      </head>
+      <body>
+      </body>
+    </html>
+    `.trim()
+
+    const virtualConsole = new jsdom.VirtualConsole()
+    jsd = new JSDOM(html, {
+      runScripts: 'dangerously',
+      resources: 'usable',
+      url: 'https://segment.com',
+      virtualConsole,
+    })
+
+    const windowSpy = jest.spyOn(global, 'window', 'get')
+    const documentSpy = jest.spyOn(global, 'document', 'get')
+
+    jest.spyOn(console, 'warn').mockImplementationOnce(() => {})
+
+    windowSpy.mockImplementation(() => {
+      return (jsd.window as unknown) as Window & typeof globalThis
+    })
+
+    documentSpy.mockImplementation(
+      () => (jsd.window.document as unknown) as Document
+    )
+  })
+
+  it('loads AJS on execution', async () => {
+    await import('../standalone')
+
+    await pWhile(
+      () => window.analytics?.initialized !== true,
+      () => {}
+    )
+
+    expect(window.analytics).not.toBeUndefined()
+    expect(window.analytics.initialized).toBe(true)
+  })
+
+  // TODO: figure out how to trigger CSP events with JSDom
+  it.skip('reverts to ajs classic in case of CSP errors', async () => {
+    await import('../standalone')
+
+    jsd.window.document.dispatchEvent(
+      new jsd.window.Event('securitypolicyviolation', {
+        // @ts-ignore jsdom doesn't implement the specific event we need :'(
+        blockedURI: 'cdn.segment.com',
+      })
+    )
+
+    const getClassic = (): HTMLScriptElement | undefined =>
+      Array.from(document.scripts).find((s) =>
+        s.getAttribute('src')?.includes('classic')
+      )
+
+    await pWhile(
+      () => getClassic() === undefined,
+      () => {}
+    )
+
+    expect(getClassic()).toMatchInlineSnapshot()
+  })
+})
--- src/standalone.ts
+++ src/standalone.ts
@@ -4,7 +4,53 @@ if (process.env.ASSET_PATH) {
   __webpack_public_path__ = process.env.ASSET_PATH
 }
 
+import { loadScript } from './lib/load-script'
 import { install } from './standalone-analytics'
+
+const regex = /(https:\/\/.*)\/analytics\.js\/v1\/(?:.*?)\/(?:platform|analytics.*)?/
+
+function getScriptPath(): string {
+  const scripts = Array.from(document.querySelectorAll('script'))
+  let path: string | undefined = undefined
+
+  for (const s of scripts) {
+    const src = s.getAttribute('src') ?? ''
+    const result = regex.exec(src)
+
+    if (result && result[1]) {
+      path = src
+      break
+    }
+  }
+
+  if (path) {
+    return path.replace('analytics.min.js', 'analytics.classic.js')
+  }
+
+  return `https://cdn.segment.com/analytics.js/v1/${window.analytics._writeKey}/analytics.classic.js`
+}
+
+let identifiedCSP = false
+
+async function onCSPError(e: SecurityPolicyViolationEvent): Promise<void> {
+  if (!e.blockedURI.includes('cdn.segment') || identifiedCSP) {
+    return
+  }
+
+  identifiedCSP = true
+
+  console.warn(
+    'Your CSP policy is missing permissions required in order to run Analytics.js 2.0'
+  )
+  console.warn('Reverting to Analytics.js 1.0')
+
+  const classicPath = getScriptPath()
+  await loadScript(classicPath)
+}
+
+document.addEventListener('securitypolicyviolation', (e) => {
+  onCSPError(e).catch(console.error)
+})
 
 // @ts-expect-error
 const isIE11 = !!window.MSInputMethodContext && !!document.documentMode
