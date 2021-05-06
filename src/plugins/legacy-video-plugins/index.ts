008fa012fe5ce000ccd766f18947fd9031e25a60
Netto Farah <nettofarah+github@gmail.com>
2021-05-06 13:27:38 -0500

    Load video plugin from npm package (#194)
    It turns out that we don't really need to load video plugins from
    a separate CDN anymore now that we have bundle splitting.
    
    By doing our own bundle splitting along with the video plugin npm
    package we still get all the benefits of not having to bundle the
    video plugin.

--- src/plugins/legacy-video-plugins/index.ts
+++ src/plugins/legacy-video-plugins/index.ts
@@ -1,16 +1,16 @@
 import { Analytics } from '../../analytics'
-import { loadScript } from '../../lib/load-script'
 
 export async function loadLegacyVideoPlugins(
   analytics: Analytics
 ): Promise<void> {
-  await loadScript(
-    'https://unpkg.com/@segment/analytics.js-video-plugins@0.2.1/dist/index.umd.js'
+  const plugins = await import(
+    // @ts-expect-error
+    '@segment/analytics.js-video-plugins/dist/index.umd.js'
   )
 
   // This is super gross, but we need to support the `window.analytics.plugins` namespace
   // that is linked in the segment docs in order to be backwards compatible with ajs-classic
 
   // @ts-expect-error
-  analytics._plugins = window.analyticsVideoPlugins
+  analytics._plugins = plugins
 }
