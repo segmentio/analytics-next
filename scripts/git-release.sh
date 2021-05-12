3b4cf3185ddfb8d4ac076ead505d68c8c54a6027
Netto Farah <nettofarah@gmail.com>
2021-05-12 13:57:34 -0500

    Remove history step

--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -14,11 +14,5 @@ version="${packageVersion}-${branch}"
 # fetch latest tags from upstream
 git fetch --tags
 
-# update changelog
-git changelog --tag v$version --no-merges
-
-# add changelog in case it's the first run
-git add History.md
-
 # tag, commit and push
 git release v$version
