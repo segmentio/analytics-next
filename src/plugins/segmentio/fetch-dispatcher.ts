dcad40888b75919f604f84e79795ed1500931035
Pooya Jaferian <pooya.j@gmail.com>
2021-11-04 14:40:52 -0700

    only use unfetch when native fetch is not available (#315)

--- src/plugins/segmentio/fetch-dispatcher.ts
+++ src/plugins/segmentio/fetch-dispatcher.ts
@@ -1,11 +1,15 @@
-import fetch from 'unfetch'
+import unfetch from 'unfetch'
+
+let fetch = unfetch
+if (typeof window !== 'undefined') {
+  fetch = window.fetch || unfetch
+}
 
 export type Dispatcher = (url: string, body: object) => Promise<unknown>
 
 export default function (): { dispatch: Dispatcher } {
   function dispatch(url: string, body: object): Promise<unknown> {
     return fetch(url, {
-      keepalive: true,
       headers: { 'Content-Type': 'text/plain' },
       method: 'post',
       body: JSON.stringify(body),
