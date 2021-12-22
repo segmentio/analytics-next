c1129f374a48618ac5d06f802f717fb634a08e84
reslear <reslear@gmail.com>
2021-12-22 13:57:34 +0300

    types: add identify reserved traits
    from
    https://segment.com/docs/connections/spec/identify/#traits
    
    complete please :)

--- src/core/events/interfaces.ts
+++ src/core/events/interfaces.ts
@@ -172,3 +172,36 @@ export interface PlanEvent {
     [key: string]: boolean
   }
 }
+
+
+export interface ReservedTraits {
+  address: Partial<{
+    city: string
+    country: string
+    postalCode: string
+    state: string
+    street: string
+  }>
+  age: number
+  avatar: string
+  birthday: Date
+  company: Partial<{
+    name: string
+    id: string | number
+    industry: string
+    employee_count: number
+  }>
+  plan: string
+  createdAt: Date
+  description: string
+  email: string
+  firstName: string
+  gender: string
+  id: string
+  lastName: string
+  name: string
+  phone: string
+  title: string
+  username: string
+  website: string
+}
