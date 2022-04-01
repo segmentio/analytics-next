13bf322a2b891d33b999640e499861ee8b4c79fd
Christopher Radek <christopher.radek@segment.com>
2022-04-01 11:28:20 -0700

    fixes qa ci job by making integrations checking more resilient

--- qa/lib/schema.ts
+++ qa/lib/schema.ts
@@ -10,19 +10,41 @@ declare global {
   }
 }
 
+function injectAncestorKeys(keys: string[]): string[] {
+  const allKeys = new Set(keys)
+  for (const key of keys) {
+    const parts = key.split('.')
+    if (parts.length === 1) continue
+
+    do {
+      // remove the last item from the parts
+      parts.pop()
+      const newKey = parts.join('.')
+      if (newKey) {
+        allKeys.add(newKey)
+      }
+    } while (parts.length)
+  }
+
+  return Array.from(allKeys)
+}
+
 export const objectSchema = (obj: object | string) => {
   let parsed = obj
   if (typeof obj === 'string') {
     parsed = JSON.parse(obj)
   }
 
-  return Object.keys(flat(parsed))
+  const flattenedKeys = Object.keys(flat(parsed))
+  const allKeys = injectAncestorKeys(flattenedKeys)
+
+  return allKeys.sort()
 }
 
 expect.extend({
   toContainSchema(got: object, expected: object) {
-    const gotSchema = objectSchema(got).sort()
-    const expectedSchema = objectSchema(expected).sort()
+    const gotSchema = objectSchema(got)
+    const expectedSchema = objectSchema(expected)
     const missing = difference(expectedSchema, gotSchema)
 
     const message = () =>
