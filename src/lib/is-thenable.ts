4d2bddf12ae8ef8e6b3e68573a56d0b8df944bdb
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-05-18 14:21:44 -0500

    update thenable

--- src/lib/is-thenable.ts
+++ src/lib/is-thenable.ts
@@ -2,10 +2,8 @@
  *  Check if  thenable
  *  (instanceof Promise doesn't respect realms)
  */
-export const isThenable = (value: unknown) =>
-  Boolean(
-    typeof value === 'object' &&
-      value !== null &&
-      'then' in value &&
-      typeof (value as any).then === 'function'
-  )
+export const isThenable = (value: unknown): boolean =>
+  typeof value === 'object' &&
+  value !== null &&
+  'then' in value &&
+  typeof (value as any).then === 'function'
