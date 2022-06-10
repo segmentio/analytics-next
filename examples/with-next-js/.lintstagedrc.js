8bef773519be80aba2cc480da96f8f621472c94d
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-06-10 15:27:39 -0500

    chore/add precommit to example, update constraints, update lint staged, remove vulnerable glob parent (#486)
    * refactor lint staged and constraints
    
    * move resolutions

---
+++ examples/with-next-js/.lintstagedrc.js
@@ -0,0 +1,11 @@
+// https://nextjs.org/docs/basic-features/eslint#lint-staged
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
