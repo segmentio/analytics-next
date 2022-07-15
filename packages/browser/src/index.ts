2e0a91c6989aa87f09c9590c5017806023664119
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-07-15 16:00:28 -0500

    Add typings instructions from snippet users and export Snippet type (#541)

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -6,3 +6,5 @@ export * from './core/context'
 export * from './core/events'
 export * from './core/plugin'
 export * from './core/user'
+
+export type { AnalyticsSnippet } from './browser/standalone-analytics'
