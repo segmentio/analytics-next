bb67ebf80441ceb075455ca908f5ecc8b4d1f4da
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-07-23 21:32:27 -0500

    add release script

--- scripts/create-release-from-tags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create github release(s) for every tag of the current commit. Use the github cli.
-
-tags=$(git tag --points-at HEAD)
-
-for tag in "${tags[@]}"; do
-  gh release create "$tag"
-done
---
+++ scripts/create-release-from-tags/index.ts
@@ -0,0 +1,179 @@
+import spawn from '@npmcli/promise-spawn'
+import getPackages from 'get-monorepo-packages'
+import path from 'path'
+import fs from 'fs'
+import { exists } from '../utils/exists'
+
+export type Config = {
+  isDryRun: boolean
+  tags: Tag[]
+}
+
+export type Tag = {
+  name: string
+  versionNumber: string
+  raw: string
+}
+
+/**
+ *
+ * @returns list of tags
+ * @example ["@segment/analytics-core@1.0.0", "@segment/analytics-next@2.1.1"]
+ */
+export const getCurrentGitTags = async (): Promise<string> => {
+  const { stdout, stderr, code } = await spawn('git', [
+    'tag',
+    '--points-at',
+    'HEAD',
+    '--column',
+  ])
+  if (code !== 0) {
+    throw new Error(stderr.toString())
+  }
+  return stdout.toString()
+}
+
+export const getConfig = async ({
+  DRY_RUN,
+  TAGS,
+}: NodeJS.ProcessEnv): Promise<Config> => {
+  const isDryRun = Boolean(DRY_RUN)
+  const tags = parseRawTags(TAGS || (await getCurrentGitTags()))
+
+  if (!tags.length) {
+    throw new Error('No git tags found.')
+  }
+  return {
+    isDryRun,
+    tags,
+  }
+}
+
+const getChangelogPath = (packageName: string): string | undefined => {
+  const result = getPackages('.').find((p) =>
+    p.package.name.includes(packageName)
+  )
+  if (!result)
+    throw new Error(`could not find package with name: ${packageName}.`)
+
+  const myPath = path.join(result.location, 'CHANGELOG.MD')
+  const pathExists = fs.existsSync(myPath)
+
+  if (pathExists) {
+    return myPath
+  } else {
+    console.log(`could not find path: ${myPath}`)
+  }
+}
+
+/**
+ *
+ * @returns list of tags
+ * @example ["@segment/analytics-core@1.0.0", "@segment/analytics-next@2.1.1"]
+ */
+const createGithubRelease = async (
+  tag: string,
+  releaseNotes?: string
+): Promise<string[]> => {
+  const { stdout, stderr, code } = await spawn('gh', [
+    'release',
+    'create',
+    tag,
+    '--notes',
+    releaseNotes || tag,
+  ])
+  if (code !== 0) {
+    throw new Error(stderr.toString())
+  }
+  return stdout.toString().trim().split(' ')
+}
+
+/**
+ *
+ * @param rawTag - ex. "@segment/analytics-foo@1.99.0"
+ */
+const extractPartsFromTag = (rawTag: string): Tag | undefined => {
+  const [name, version] = rawTag.split(/@(\d.*)/)
+  if (!name || !version) return undefined
+  return {
+    name,
+    versionNumber: version?.replace('\n', '') as string,
+    raw: rawTag,
+  }
+}
+
+export const parseRawTags = (rawTags: string): Tag[] => {
+  return rawTags.trim().split(' ').map(extractPartsFromTag).filter(exists)
+}
+
+/**
+ *
+ * @returns the release notes that correspond to a given tag.
+ */
+const parseReleaseNotes = (
+  changelogText: string,
+  versionNumber: string
+): string => {
+  const re = /(##\s.*\d.*)/gi
+  let begin: number
+  let end: number
+
+  changelogText.split('\n').forEach((el, idx) => {
+    if (begin && end) return
+    if (el.includes(versionNumber)) {
+      begin = idx + 1
+    } else if (begin && re.test(el)) {
+      end = idx - 1
+    }
+  })
+
+  const result = changelogText.split('\n').filter((_, idx) => {
+    return idx >= begin && idx <= end
+  })
+  return result.join('\n')
+}
+
+const getReleaseNotes = (tag: Tag): string | undefined => {
+  const { name, versionNumber } = tag
+  const changelogPath = getChangelogPath(name)
+  if (!changelogPath) {
+    console.log(`no changelog path for ${name}... skipping.`)
+    return
+  }
+  const changelogText = fs.readFileSync(changelogPath, { encoding: 'utf8' })
+  const releaseNotes = parseReleaseNotes(changelogText, versionNumber)
+  if (!releaseNotes) {
+    console.log(
+      `Could not find release notes for tags ${tag.raw} in ${changelogPath}.`
+    )
+  }
+  return releaseNotes
+}
+
+const createGithubReleaseFromTag = async (
+  tag: Tag,
+  { dryRun = false } = {}
+): Promise<void> => {
+  const notes = getReleaseNotes(tag)
+  if (notes) {
+    console.log(
+      `\n ---> Outputting release titled: ${tag.raw} with notes: \n ${notes}`
+    )
+  }
+
+  if (dryRun) {
+    console.log(`--> Dry run: ${tag.raw} not released.`)
+    return undefined
+  }
+
+  await createGithubRelease(tag.raw, notes)
+  return undefined
+}
+
+export const createReleaseFromTags = async (config: Config) => {
+  console.log('Processing tags:', config.tags, '\n')
+
+  for (const tag of config.tags) {
+    await createGithubReleaseFromTag(tag, { dryRun: config.isDryRun })
+  }
+}
