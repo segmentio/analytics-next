8f65542e47bdd052bf4d4b509ed402e6bd4b37a5
Seth Silesky <seth.silesky@gmail.com>
2022-09-20 18:49:45 -0500

    Improve doc strings and add typedef tests

--- packages/node/src/__tests__/typedef-tests.ts
+++ packages/node/src/__tests__/typedef-tests.ts
@@ -1,4 +1,6 @@
+import { method } from 'lodash'
 import { AnalyticsNode } from '../'
+import { analyticsNode } from '../app/plugin'
 
 /**
  * These are general typescript definition tests;
@@ -12,5 +14,36 @@ export default {
 
     // @ts-expect-error - should not be possible
     analytics.VERSION = 'foo'
+  },
+
+  'track/id/pg/screen/grp calls should require either userId or anonymousId':
+    () => {
+      const analytics = new AnalyticsNode({ writeKey: 'abc' })
+      const method: 'track' | 'identify' | 'page' | 'screen' | 'group' = 'track'
+
+      // @ts-expect-error - no userID
+      analytics[method]({ event: 'foo' })
+
+      analytics[method]({ event: 'foo', userId: 'bar' })
+      analytics[method]({ event: 'foo', anonymousId: 'bar' })
+      analytics[method]({ event: 'foo', anonymousId: 'bar', userId: 'bar' })
+    },
+
+  'alias does not need a userId': () => {
+    const analytics = new AnalyticsNode({ writeKey: 'abc' })
+
+    // @ts-expect-error - no userId
+    analytics.alias({ previousId: 'old_id_either_anon_or_regular' })
+
+    analytics.alias({
+      userId: 'new',
+      previousId: 'old_id_either_anon_or_regular',
+    })
+
+    analytics.alias({
+      // @ts-expect-error - anonymousId is not valid
+      anonymousId: 'old_id_either_anon_or_regular',
+      previousId: 'foo',
+    })
   },
 }
