4e88c902ad3b670c13f0b208c0ac87ed5be748de
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-11-03 17:36:23 -0500

    add node rc publish script

---
+++ packages/node/scripts/prerelease.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Run this script on master when ready to publish node.
+
+echo "bumping version..." &&
+  npm version prerelease &&
+  git add package.json &&
+  git commit -m "bump node version"
+
+echo "modifying package.json..." &&
+  # delete private field from package.json
+  echo "$(jq 'del(.private)' package.json)" >package.json &&
+  # add repository info to package.json
+  yarn constraints --fix
+
+echo "building and publishing..." &&
+  yarn build &&
+  npm publish --tag rc &&
+  git push
+
+echo "cleaning up" &&
+  git checkout .
