c2949d4ca030b4e89683b65ba06316495bcf23a8
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-11-16 16:46:22 -0600

    export Analytics as both a named export and a default export (#677)

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,2 +1,6 @@
 export * from './app/analytics-node'
 export type { AnalyticsSettings } from './app/settings'
+
+// export Analytics as both a named export and a default export (for backwards-compat. reasons)
+import { Analytics } from './app/analytics-node'
+export default Analytics
