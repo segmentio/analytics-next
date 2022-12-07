0bc43b523623b79f46c20be754a0a51bbbdbd831
Christopher Radek <christopher.radek@segment.com>
2022-12-06 23:27:45 -0800

    refactor standalone mock

--- packages/browser-integration-tests/src/index.test.ts
+++ packages/browser-integration-tests/src/index.test.ts
@@ -1,54 +1,9 @@
-import { join as joinPath } from 'path'
 import { test, expect } from '@playwright/test'
 import { SettingsBuilder } from './fixtures/settings'
+import { standaloneMock } from './helpers/standalone-mock'
 
 test.describe('Standalone tests', () => {
-  test.beforeEach(async ({ context }) => {
-    // Setup routing to monorepo
-    const ajsBasePath = joinPath(
-      __dirname,
-      '..',
-      'node_modules',
-      '@segment',
-      'analytics-next',
-      'dist',
-      'umd'
-    )
-    await context.route(
-      'https://cdn.segment.com/analytics.js/v1/*/analytics.min.js',
-      (route, request) => {
-        if (request.method().toLowerCase() !== 'get') {
-          return route.continue()
-        }
-
-        return route.fulfill({
-          path: joinPath(ajsBasePath, 'standalone.js'),
-          status: 200,
-          headers: {
-            'Content-Type': 'text/javascript',
-          },
-        })
-      }
-    )
-
-    await context.route(
-      'https://cdn.segment.com/analytics-next/bundles/*',
-      (route, request) => {
-        if (request.method().toLowerCase() !== 'get') {
-          return route.continue()
-        }
-
-        const filename = request.url().split('/').pop()!
-        return route.fulfill({
-          path: joinPath(ajsBasePath, filename),
-          status: 200,
-          headers: {
-            'Content-Type': 'text/javascript',
-          },
-        })
-      }
-    )
-  })
+  test.beforeEach(standaloneMock)
 
   test.describe('Actions Amplitude', () => {
     test.beforeEach(async ({ context }) => {
@@ -195,6 +150,63 @@ test.describe('Standalone tests', () => {
         'Braze Cloud Mode (Actions)': true,
         'Braze Web Mode (Actions)': true,
       })
+    })
+  })
+
+  test.describe('Segment.io Retries', () => {
+    test.beforeEach(async ({ context }) => {
+      await context.route(
+        'https://cdn.segment.com/v1/projects/*/settings',
+        (route, request) => {
+          if (request.method().toLowerCase() !== 'get') {
+            return route.continue()
+          }
+
+          return route.fulfill({
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(new SettingsBuilder().build()),
+          })
+        }
+      )
+    })
+
+    test.skip('supports retries on page navigation', async ({ page }) => {
+      // Load analytics.js
+      await page.goto('/standalone.html')
+      await page.evaluate(() => window.analytics.load('fake-key'))
+
+      // fail the 1st request
+      await page.route(
+        'https://api.segment.io/v1/t',
+        (route) => {
+          return route.abort('blockedbyclient')
+        },
+        {
+          times: 1,
+        }
+      )
+      const requestFailure = new Promise((resolve) => {
+        page.once('requestfailed', resolve)
+      })
+
+      // trigger an event
+      await page.evaluate(() => {
+        void window.analytics.track('test event')
+      })
+
+      await requestFailure
+      await page.reload()
+
+      // load analytics.js again and wait for a new request.
+      const [request] = await Promise.all([
+        page.waitForRequest('https://api.segment.io/v1/t'),
+        page.evaluate(() => window.analytics.load('fake-key')),
+      ])
+
+      expect(request.method()).toBe('GET')
     })
   })
 })
