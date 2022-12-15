9e748a552676439a8256d2b7a3e13915ef1d5da0
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-12-15 10:33:12 -0600

    node: improve typescript error messages for parameters (#728)

--- packages/node/src/app/types/params.ts
+++ packages/node/src/app/types/params.ts
@@ -39,21 +39,21 @@ export type AliasParams = {
   integrations?: Integrations
 }
 
-export type GroupParams = IdentityOptions & {
+export type GroupParams = {
   groupId: string
   traits?: Traits
   context?: ExtraContext
   timestamp?: Timestamp
   integrations?: Integrations
-}
+} & IdentityOptions
 
-export type IdentifyParams = IdentityOptions & {
+export type IdentifyParams = {
   traits?: Traits
   context?: ExtraContext
   integrations?: Integrations
-}
+} & IdentityOptions
 
-export type PageParams = IdentityOptions & {
+export type PageParams = {
   /*  The category of the page. Useful for cases like ecommerce where many pages might live under a single category. */
   category?: string
   /* The name of the page.*/
@@ -63,12 +63,12 @@ export type PageParams = IdentityOptions & {
   timestamp?: Timestamp
   context?: ExtraContext
   integrations?: Integrations
-}
+} & IdentityOptions
 
-export type TrackParams = IdentityOptions & {
+export type TrackParams = {
   event: string
   properties?: EventProperties
   context?: ExtraContext
   timestamp?: Timestamp
   integrations?: Integrations
-}
+} & IdentityOptions
