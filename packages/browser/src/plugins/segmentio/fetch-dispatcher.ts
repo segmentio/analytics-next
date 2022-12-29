d8af6fc3d17546faf7ab0f68cb8bc57dc23ee69e
Krishnamurti Subramanian <krishnamurti_subramanian@@intuit.com>
2022-12-29 00:39:54 -0800

    fix: fetch-dispatcher to set fetch within dispatch

--- packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
+++ packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
@@ -1,14 +1,13 @@
 import unfetch from 'unfetch'
 
-let fetch = unfetch
-if (typeof window !== 'undefined') {
-  fetch = window.fetch || unfetch
-}
-
 export type Dispatcher = (url: string, body: object) => Promise<unknown>
 
 export default function (): { dispatch: Dispatcher } {
   function dispatch(url: string, body: object): Promise<unknown> {
+    let fetch = unfetch
+    if (typeof window !== 'undefined') {
+      fetch = window.fetch || unfetch
+    }
     return fetch(url, {
       headers: { 'Content-Type': 'text/plain' },
       method: 'post',
