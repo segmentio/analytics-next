2b57cb39f652876b8f6954fb93b245828977ab5d
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-12-31 16:46:43 -0600

    add additional test

--- packages/browser/src/lib/__tests__/fetch.test.ts
+++ packages/browser/src/lib/__tests__/fetch.test.ts
@@ -11,15 +11,24 @@ const getGlobalMock = jest.mocked(getGlobal)
 describe(fetch, () => {
   const testFetchArgs = ['http://foo.com', {}] as const
 
-  it('should call native fetch if available', async () => {
-    const nativeFetchSpy = jest.fn()
-    getGlobalMock.mockReturnValue({ ...window, fetch: nativeFetchSpy })
+  it('should call native fetch if available', () => {
+    const nativeFetchMock = jest.fn()
+    getGlobalMock.mockReturnValue({ ...window, fetch: nativeFetchMock })
     void fetch(...testFetchArgs)
-    expect(nativeFetchSpy).toBeCalledWith(...testFetchArgs)
+    expect(nativeFetchMock).toBeCalledWith(...testFetchArgs)
     expect(unfetchMock).not.toBeCalled()
   })
-  it('should fall back to unfetch', async () => {
+  it('should fall back to unfetch in non-browserlike environment', () => {
     getGlobalMock.mockReturnValue(null)
+    void fetch(...testFetchArgs)
+    expect(unfetchMock).toBeCalledWith(...testFetchArgs)
+  })
+  it('should fall back to unfetch if native fetch is unsupported', () => {
+    getGlobalMock.mockReturnValue({
+      ...window,
+      fetch: undefined,
+    } as any)
+
     void fetch(...testFetchArgs)
     expect(unfetchMock).toBeCalledWith(...testFetchArgs)
   })
