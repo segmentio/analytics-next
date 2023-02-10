037de611578559db0adf8c1c4e7167f6a104d662
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-02-09 22:32:33 -0600

    tweak config

--- packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
+++ packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
@@ -5,12 +5,12 @@ export type Dispatcher = (url: string, body: object) => Promise<unknown>
 export interface FetchConfig {
   keepalive?: boolean
 }
-export default function (config: FetchConfig = {}): {
+export default function (config?: FetchConfig): {
   dispatch: Dispatcher
 } {
   function dispatch(url: string, body: object): Promise<unknown> {
     return fetch(url, {
-      keepalive: config.keepalive,
+      keepalive: config?.keepalive,
       headers: { 'Content-Type': 'text/plain' },
       method: 'post',
       body: JSON.stringify(body),
