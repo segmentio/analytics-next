171cd15b4c32f4c0780c1e3d8d6d646bb17c6c36
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-02-09 18:25:03 -0600

    add keep alive option to fetch standard transport (#787)

--- packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
+++ packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
@@ -5,6 +5,7 @@ export type Dispatcher = (url: string, body: object) => Promise<unknown>
 export default function (): { dispatch: Dispatcher } {
   function dispatch(url: string, body: object): Promise<unknown> {
     return fetch(url, {
+      keepalive: true,
       headers: { 'Content-Type': 'text/plain' },
       method: 'post',
       body: JSON.stringify(body),
