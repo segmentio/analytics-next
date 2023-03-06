c0fb121e22b16443b5d35437f3bc54d2c6c7d93d
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-03-06 13:06:23 -0600

    update yarn

--- scripts/update-lockfile.sh
+++ scripts/update-lockfile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Changesets does not support yarn, so if the resolutions change, we want to commit them as part of the version pipeline.
+# # Changesets does not support yarn, so if the resolutions change, we want to commit them as part of the version pipeline.
+
 echo "pwd: $(pwd)"
 echo "Checking if yarn.lock is up-to-date"
-YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-3.2.1.cjs # https://github.com/changesets/action/issues/170
+
+yarn_path=$(grep "yarnPath:" .yarnrc.yml | awk 'NF>1{print $NF}')
+YARN_ENABLE_IMMUTABLE_INSTALLS=false node "$yarn_path" # https://github.com/changesets/action/issues/170
 git add yarn.lock
 git status --porcelain
+
