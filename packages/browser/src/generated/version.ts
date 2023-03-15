f97c1492b0ae893f3d95bd99d7c73912bf1618eb
Osvaldo Gama <osvaldo@metarouter.io>
2023-03-15 16:33:55 -0500

    passing trace as header

--- packages/browser/src/generated/version.ts
+++ packages/browser/src/generated/version.ts
@@ -1,2 +1,2 @@
 // This file is generated.
-export const version = '1.51.3-mr.1'
+export const version = '1.51.3-mr.2'
--- packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
+++ packages/browser/src/plugins/segmentio/fetch-dispatcher.ts
@@ -4,15 +4,23 @@ export type Dispatcher = (url: string, body: object) => Promise<unknown>
 
 export type StandardDispatcherConfig = {
   keepalive?: boolean
+  trace?: boolean
 }
 
 export default function (config?: StandardDispatcherConfig): {
   dispatch: Dispatcher
 } {
   function dispatch(url: string, body: object): Promise<unknown> {
+    let traceString = 'false'
+    if (config?.trace) {
+      traceString = config?.trace ? 'true' : 'false'
+    }
     return fetch(url, {
       keepalive: config?.keepalive,
-      headers: { 'Content-Type': 'text/plain' },
+      headers: {
+        'Content-Type': 'text/plain',
+        Trace: traceString,
+      },
       method: 'post',
       body: JSON.stringify(body),
     })
