4027accb977bf16df71c054cc65d18298d5e78a7
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-04-11 18:21:46 -0500

    share strings

--- packages/core/src/validation/assertions.ts
+++ packages/core/src/validation/assertions.ts
@@ -2,6 +2,10 @@ import { CoreSegmentEvent } from '../events'
 import { ValidationError } from './errors'
 import { isString, isPlainObject, exists } from './helpers'
 
+const stringError = 'is not a string'
+const objError = 'is not an object'
+const nilError = 'is nil'
+
 export function assertUserIdentity(event: CoreSegmentEvent): void {
   const USER_FIELD_NAME = '.userId/anonymousId/previousId/groupId'
 
@@ -10,9 +14,9 @@ export function assertUserIdentity(event: CoreSegmentEvent): void {
 
   const id = getAnyUserId(event)
   if (!exists(id)) {
-    throw new ValidationError(USER_FIELD_NAME, 'is not defined')
+    throw new ValidationError(USER_FIELD_NAME, nilError)
   } else if (!isString(id)) {
-    throw new ValidationError(USER_FIELD_NAME, 'is not a string')
+    throw new ValidationError(USER_FIELD_NAME, stringError)
   }
 }
 
@@ -20,31 +24,31 @@ export function assertEventExists(
   event?: CoreSegmentEvent | null
 ): asserts event is CoreSegmentEvent {
   if (!event || typeof event !== 'object') {
-    throw new ValidationError('Event', 'is nil')
+    throw new ValidationError('Event', nilError)
   }
 }
 
 export function assertEventType(event: CoreSegmentEvent): void {
   if (!isString(event.type)) {
-    throw new ValidationError('.type', 'is not a string')
+    throw new ValidationError('.type', stringError)
   }
 }
 
 export function assertTrackEventName(event: CoreSegmentEvent): void {
   if (!isString(event.event)) {
-    throw new ValidationError('.event', 'is not a string')
+    throw new ValidationError('.event', stringError)
   }
 }
 
 export function assertTrackEventProperties(event: CoreSegmentEvent): void {
   if (!isPlainObject(event.properties)) {
-    throw new ValidationError('.properties', 'is not an object')
+    throw new ValidationError('.properties', objError)
   }
 }
 
 export function assertTraits(event: CoreSegmentEvent): void {
   if (!isPlainObject(event.traits)) {
-    throw new ValidationError('.traits', 'is not an object')
+    throw new ValidationError('.traits', objError)
   }
 }
 
