8d0860d1a8fd7e6a2cc2fc9c8e9529451d2494ab
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-27 22:10:33 -0600

    wip

--- packages/consent/consent-tools/src/utils/pick.ts
+++ packages/consent/consent-tools/src/utils/pick.ts
@@ -2,14 +2,14 @@
  * @example
  * pick({ a: 1, b: 2, c: 3 }, ['a', 'c']) => { a: 1, c: 3 }
  */
-export const pick = <T extends Record<string, any>, K extends string>(
-  obj: T,
-  keys: K[]
-): Pick<T, K> => {
+export const pick = <Obj extends Record<Key, any>, Key extends string>(
+  obj: Obj,
+  keys: Key[]
+): Pick<Obj, Key> => {
   return keys.reduce((acc, k) => {
     if (k in obj) {
       ;(acc as any)[k] = obj[k]
     }
     return acc
-  }, {} as T)
+  }, {} as Obj)
 }
