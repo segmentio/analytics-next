d3d694fd655275fc3bb977a9376a9749767e1673
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-07-12 14:19:21 -0500

    wip

--- packages/node/src/lib/http-client.ts
+++ packages/node/src/lib/http-client.ts
@@ -54,7 +54,7 @@ export interface HTTPClientRequest {
    */
   data: Record<string, any>
   /**
-   * Specifies the timeout (in milliseconds) for an HTTP client to get an HTTP response from HTTP server
+   * Specifies the timeout (in milliseconds) for an HTTP client to get an HTTP response from the server
    * @example 10000
    */
   httpRequestTimeout: number
