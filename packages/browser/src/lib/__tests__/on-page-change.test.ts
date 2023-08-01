7072377c0adae54b844a441fa545763e0ff654ba
dsjackins <danieljackins@gmail.com>
2023-08-01 13:29:34 -0600

    Fix batching after page navigation (#915)
    Co-authored-by: Daniel Jackins <djackins@twilio.com>

--- packages/browser/src/lib/__tests__/on-page-change.test.ts
+++ packages/browser/src/lib/__tests__/on-page-change.test.ts
@@ -1,4 +1,4 @@
-import { onPageLeave } from '../on-page-leave'
+import { onPageChange } from '../on-page-change'
 
 const helpers = {
   dispatchEvent(event: 'pagehide' | 'visibilitychange') {
@@ -24,42 +24,51 @@ beforeEach(() => {
   helpers.setVisibilityState('visible')
 })
 
-describe('onPageLeave', () => {
+describe('onPageChange', () => {
   test('callback should fire on pagehide', () => {
     const cb = jest.fn()
-    onPageLeave(cb)
+    onPageChange(cb)
     helpers.dispatchPageHideEvent()
     expect(cb).toBeCalledTimes(1)
   })
 
   test('callback should fire if document becomes hidden', () => {
     const cb = jest.fn()
-    onPageLeave(cb)
+    onPageChange(cb)
     helpers.dispatchVisChangeEvent('hidden')
     expect(cb).toBeCalledTimes(1)
   })
 
-  test('callback should *not* fire if document becomes visible', () => {
+  test('callback should fire if document becomes visible', () => {
     const cb = jest.fn()
-    onPageLeave(cb)
+    onPageChange(cb)
     helpers.dispatchVisChangeEvent('visible')
-    expect(cb).not.toBeCalled()
+    expect(cb).toBeCalledTimes(1)
   })
 
   test('if both event handlers fire, callback should still fire only once', () => {
     const cb = jest.fn()
-    onPageLeave(cb)
+    onPageChange(cb)
     helpers.dispatchVisChangeEvent('hidden')
     helpers.dispatchPageHideEvent()
     expect(cb).toBeCalledTimes(1)
   })
 
-  test('if user leaves a tab, returns, and leaves again, callback should be called on each departure', () => {
+  test('if user leaves a tab, returns, and leaves again, callback should be called on each navigation', () => {
     const cb = jest.fn()
-    onPageLeave(cb)
+    onPageChange(cb)
     helpers.dispatchVisChangeEvent('hidden')
     helpers.dispatchVisChangeEvent('visible')
     helpers.dispatchVisChangeEvent('hidden')
-    expect(cb).toBeCalledTimes(2)
+    expect(cb).toBeCalledTimes(3)
+  })
+
+  test('if user navigates, callback should be passed appropriate "unloaded" value', () => {
+    const cb = jest.fn()
+    onPageChange(cb)
+    helpers.dispatchVisChangeEvent('hidden')
+    expect(cb).toBeCalledWith(true)
+    helpers.dispatchVisChangeEvent('visible')
+    expect(cb).toBeCalledWith(false)
   })
 })
