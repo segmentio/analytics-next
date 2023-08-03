9b4cb0ffeea7c17bc0479b3bb23fa12504a376c2
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-03 18:31:02 -0500

    wip

--- examples/with-consent-next-js/.env.local.EXAMPLE.js
+++ examples/with-consent-next-js/.env.local.EXAMPLE.js
@@ -1,5 +1,5 @@
 module.exports = {
-  WRITEKEY: undefined, // can also pass in query string
+  WRITEKEY: undefined, // can also pass in query string (e.g. ?writeKey=1234)
   ONE_TRUST_OPTIONS: {
     integrationCategoryMappings: {
       Fullstory: ['C0001', 'NO_MATCH'],
