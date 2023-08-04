51d0307b0f923c01b99b3de52ef92eb9048b2c15
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-04 17:44:55 -0500

    wip

--- examples/with-consent-next-js/.env.local.EXAMPLE.js
+++ examples/with-consent-next-js/.env.local.EXAMPLE.js
@@ -1,10 +1,5 @@
 module.exports = {
   WRITEKEY: undefined, // can also pass in query string (e.g. ?writeKey=1234)
-  ONETRUST_API_KEY: "80ca7b5c-e72f-4bd0-972a-b74d052a0820-test",
-  ONE_TRUST_OPTIONS: {
-    integrationCategoryMappings: {
-      Fullstory: ['C0001', 'NO_MATCH'],
-      'Braze Web Mode (Actions)': ['C0004', 'C0003'],
-    },
-  },
+  ONETRUST_API_KEY: undefined,
+  ONE_TRUST_OPTIONS: {},
 }
