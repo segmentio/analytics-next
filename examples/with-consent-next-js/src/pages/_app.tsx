4abbc1b440d615b50418b44018abaded35aa750d
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-09 16:40:54 -0500

    wip

--- examples/with-consent-next-js/src/pages/_app.tsx
+++ examples/with-consent-next-js/src/pages/_app.tsx
@@ -2,5 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // @ts-ignore - unknown build error, likely from a TS version mismatch
   return <Component {...pageProps} />
 }
