e1f9ede1ce97bfec9f64366e81bbc05732bfbedb
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-09 15:16:32 -0500

    wip

--- examples/with-consent-next-js/src/pages/_app.tsx
+++ examples/with-consent-next-js/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useAnalyticsPageEvent } from '../utils/hooks/analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useAnalyticsPageEvent()
+  // @ts-ignore
   return <Component {...pageProps} />
 }
--- examples/with-consent-next-js/src/pages/_document.tsx
+++ examples/with-consent-next-js/src/pages/_document.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useAnalyticsPageEvent } from '../utils/hooks/analytics'
 import { getConfig } from '../utils/config'
 export default function Document() {
-  useAnalyticsPageEvent()
   return (
     <Html lang="en">
       <Head />
