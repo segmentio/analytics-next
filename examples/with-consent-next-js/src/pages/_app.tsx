78617eda9d86bb81675faab8ec331364c4a8b640
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-09 16:20:31 -0500

    wip

--- examples/with-consent-next-js/src/pages/_app.tsx
+++ examples/with-consent-next-js/src/pages/_app.tsx
@@ -4,6 +4,5 @@ import { useAnalyticsPageEvent } from '../utils/hooks/analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   useAnalyticsPageEvent()
-  // @ts-ignore
   return <Component {...pageProps} />
 }
