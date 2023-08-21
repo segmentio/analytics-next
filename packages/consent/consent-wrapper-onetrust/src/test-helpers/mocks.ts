cb0cbada3f34d4f728be6e66bee60faee9f97ce1
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-21 16:18:23 -0500

    wip

--- packages/consent/consent-wrapper-onetrust/src/test-helpers/mocks.ts
+++ packages/consent/consent-wrapper-onetrust/src/test-helpers/mocks.ts
@@ -16,8 +16,8 @@ export const OneTrustMockGlobal: jest.Mocked<OneTrustGlobal> = {
 }
 
 export const analyticsMock: jest.Mocked<AnyAnalytics> = {
-  addSourceMiddleware: jest.fn(),
-  load: jest.fn(),
-  on: jest.fn(),
-  track: jest.fn(),
+  addSourceMiddleware: jest.fn().mockImplementation(throwNotImplemented),
+  load: jest.fn().mockImplementation(throwNotImplemented),
+  on: jest.fn().mockImplementation(throwNotImplemented),
+  track: jest.fn().mockImplementation(throwNotImplemented),
 }
