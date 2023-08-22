ddec874592c50ecab1d4017462d2da17af659476
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-08-22 13:00:18 -0500

    wip

--- packages/consent/consent-tools/src/domain/validation/validation-error.ts
+++ packages/consent/consent-tools/src/domain/validation/validation-error.ts
@@ -4,7 +4,7 @@ export class ValidationError extends AnalyticsConsentError {
   constructor(message: string, received?: any) {
     if (arguments.length === 2) {
       // to ensure that explicitly passing undefined as second argument still works
-      message += `(Received: ${JSON.stringify(received)})`
+      message += ` (Received: ${JSON.stringify(received)})`
     }
     super('ValidationError', `[Validation] ${message}`)
   }
