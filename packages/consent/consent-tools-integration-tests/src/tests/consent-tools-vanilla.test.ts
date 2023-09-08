91a0f978221c488768fdf9e3c9842fbd2535749d
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-09-07 21:20:03 -0500

    wip

--- packages/consent/consent-tools-integration-tests/src/tests/consent-tools-vanilla.test.ts
+++ packages/consent/consent-tools-integration-tests/src/tests/consent-tools-vanilla.test.ts
@@ -1,28 +1,17 @@
 import page from '../page-objects/consent-tools-vanilla'
 import { expect } from 'expect'
-import { Context } from '@segment/analytics-next'
 
 // Verify that the consent tools wrapper is working as expected (no OneTrust)
 it('should stamp each event', async () => {
   await page.load()
-  const commands = [
-    `analytics.track("hello world")`,
-    `analytics.alias("foo", "bar")`,
-    `analytics.page()`,
-    `analytics.group("foo", { bar: 123 })`,
-    `analytics.identify("bar", { bar: 123 })`,
-  ]
-
-  const responses = await Promise.all<Context>(
-    commands.map((cmd) => browser.execute(`return ${cmd}`))
+  const responses = await browser.execute(() =>
+    window.analytics.track('hello world')
   )
 
-  responses.forEach((ctx) => {
-    expect((ctx.event.context as any).consent).toEqual({
-      categoryPreferences: {
-        Advertising: true,
-        Analytics: true,
-      },
-    })
+  expect((responses.event.context as any).consent).toEqual({
+    categoryPreferences: {
+      Advertising: true,
+      Analytics: true,
+    },
   })
 })
