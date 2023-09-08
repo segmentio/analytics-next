b7515a94f6b16ae8909e82c041776dc0daf01457
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-09-07 21:40:27 -0500

    wip

--- packages/consent/consent-tools-integration-tests/wdio.conf.local.ts
+++ packages/consent/consent-tools-integration-tests/wdio.conf.local.ts
@@ -54,7 +54,7 @@ export const config: Options.Testrunner = {
       maxInstances: 5,
       browserName: 'chrome',
       'goog:chromeOptions': {
-        args: ['disable-gpu'],
+        args: ['headless', 'disable-gpu'],
       },
       acceptInsecureCerts: true,
     },
