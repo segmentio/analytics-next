fcdfcca23b770b49b7d1955ab4b3b3cc6fbe1746
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-09-27 13:47:28 -0500

    wip

--- packages/browser/src/core/page/add-page-context.ts
+++ packages/browser/src/core/page/add-page-context.ts
@@ -3,10 +3,10 @@ import { EventProperties, SegmentEvent } from '../events'
 import { getDefaultPageContext } from './get-page-context'
 
 /**
- *
  * Augments a segment event with information about the current page.
  * Page information like URL changes frequently, so this is meant to be captured as close to the event call as possible.
- * Things like `userAgent` do not change, so they can be augmented later in the flow.
+ * Things like `userAgent` do not change, so they can be added later in the flow.
+ * We prefer not to add this information to this function, as it increases the main bundle size.
  */
 export const addPageContext = (
   event: SegmentEvent,
