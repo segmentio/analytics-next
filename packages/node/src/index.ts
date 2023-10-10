8d7521083ade2579d10e840be239c35b943b7ee4
Christopher Radek <christopher.radek@segment.com>
2023-10-10 10:31:16 -0700

    create common index file

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,30 +1,7 @@
-export { Analytics } from './app/analytics-node'
-export { Context } from './app/context'
-export {
-  HTTPClient,
-  FetchHTTPClient,
-  HTTPFetchRequest,
-  HTTPResponse,
-  HTTPFetchFn,
-  HTTPClientRequest,
-} from './lib/http-client'
-
-export { OAuthSettings } from './lib/types'
-
-export type {
-  Plugin,
-  GroupTraits,
-  UserTraits,
-  TrackParams,
-  IdentifyParams,
-  AliasParams,
-  GroupParams,
-  PageParams,
-} from './app/types'
-export type { AnalyticsSettings } from './app/settings'
+export * from './index.common'
 
 // export Analytics as both a named export and a default export (for backwards-compat. reasons)
-import { Analytics } from './app/analytics-node'
+import { Analytics } from './index.common'
 export default Analytics
 
 import { dependencyInjection } from './lib/dependency-injection'
