15b55080f98734cae49e5ef13a395cc5a22e268a
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-10-27 12:02:37 -0500

    Update utils.ts

--- packages/consent/consent-wrapper-onetrust/src/test-helpers/utils.ts
+++ packages/consent/consent-wrapper-onetrust/src/test-helpers/utils.ts
@@ -2,7 +2,7 @@ export const addMockImplementation = (mock: jest.Mocked<any>) => {
   Object.entries(mock).forEach(([method, value]) => {
     // automatically add mock implementation for debugging purposes
     if (typeof value === 'function') {
-      mock[method] = mock[method].mockImplementation((args: any) => {
+      mock[method] = mock[method].mockImplementation((...args: any[]) => {
         throw new Error(`Not Implemented: ${method}(${JSON.stringify(args)})`)
       })
     }
