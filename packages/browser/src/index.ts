db8e044dd694568eff1a9f8e265756df2fe68552
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-04-16 16:36:26 -0500

    wip

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -8,10 +8,6 @@ export * from './core/plugin'
 export * from './core/user'
 
 export type { AnalyticsSnippet } from './browser/standalone-interface'
-export type {
-  MiddlewareFunction,
-  DestinationMiddlewareFunction,
-  MiddlewareParams,
-} from './plugins/middleware'
+export type { MiddlewareFunction } from './plugins/middleware'
 export { getGlobalAnalytics } from './lib/global-analytics-helper'
 export { UniversalStorage, Store, StorageObject } from './core/storage'
