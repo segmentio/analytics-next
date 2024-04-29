51a08cfda439dcbb9d130a5da74f5ac013fceccc
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-04-29 15:36:34 -0500

    wip

--- packages/browser/src/plugins/remote-loader/index.ts
+++ packages/browser/src/plugins/remote-loader/index.ts
@@ -109,6 +109,7 @@ export class ActionDestination implements InternalPluginWithAddMiddleware {
           type: 'action',
           didError: true,
         })
+        throw error
       }
 
       return ctx
