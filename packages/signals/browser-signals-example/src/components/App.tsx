7a19b38a3d938af6c489a4df9313c9496198e829
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-05-24 17:03:56 -0500

    add example app

---
+++ packages/signals/browser-signals-example/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComplexForm from './ComplexForm'
+const App: React.FC = () => {
+  return (
+    <main>
+      <h1>Hello, React with TypeScript!</h1>
+      <ComplexForm />
+    </main>
+  )
+}
+
+export default App
---
+++ packages/signals/browser-signals-example/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+/**
+ * This is a base webpack config that is used for all generic web packages.
+ * It should contain the same support as analytics.js (e.g. es5, minified, etc)
+ *
+ *
+ * @type { import('webpack').Configuration }
+ */
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+}
