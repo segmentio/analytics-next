6aaab9b5bb136beb7b0cbd343bc99299d16d8afb
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-07-16 12:10:50 -0500

    refactor to not use enum

--- packages/signals/signals-integration-tests/src/tests/signals-vanilla/index-page.ts
+++ packages/signals/signals-integration-tests/src/tests/signals-vanilla/index-page.ts
@@ -4,7 +4,7 @@ import { promiseTimeout } from '@internal/test-helpers'
 const edgeFn = `
     // this is a process signal function
     const processSignal = (signal) => {
-      if (signal.type === SignalType.Interaction) {
+      if (signal.type === 'interaction') {
         const eventName = signal.data.eventType + ' ' + '[' + signal.type + ']'
         analytics.track(eventName, signal.data)
       }
