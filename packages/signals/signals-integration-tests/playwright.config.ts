8357a6bc2ac363054279749ac0ec26bd0c2b3e80
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-09-21 18:42:51 -0500

    wip

--- packages/signals/signals-integration-tests/playwright.config.ts
+++ packages/signals/signals-integration-tests/playwright.config.ts
@@ -31,13 +31,11 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'list',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
