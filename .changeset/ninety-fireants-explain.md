c45d445beb1b1d5b03738557720720d05e9c08a3
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-09-25 10:48:46 -0500

    Do not make plugin.version mandatory (#1128)

---
+++ .changeset/ninety-fireants-explain.md
@@ -0,0 +1,7 @@
+---
+'@segment/analytics-core': minor
+'@segment/analytics-next': minor
+'@segment/analytics-node': minor
+---
+
+Do not make plugin.version required
--- packages/core/src/plugins/index.ts
+++ packages/core/src/plugins/index.ts
@@ -17,8 +17,18 @@ export interface CorePlugin<
   Analytics extends CoreAnalytics = any
 > {
   name: string
+
+  /**
+   * A concatenation of the current destination name and the specific action. This field is only relevant for destination plugins.
+   * @example ['Braze Web Mode (Actions) updateUserProfile'] // (and the plugin name would be 'Braze Web Mode (Actions)')
+   */
+  // This is only used for disabling action plugins. An action destination plugin name used to be the concatenation of the current destination name, and the specific action (like 'Braze Web Mode (Actions) updateUserProfile'. Which meant customers had to set that individual action to false to disable it before. Then with the destination filter route, we created a new wrapper around actions to make it easy to disable based on the creationName, which is when we added creationName to the objects in remotePlugin. However, to preserve backwards compatibility, we still needed to support disabling based on the concatenation since customers were doing that in the wild, so we can filter based on checking both name fields.
   alternativeNames?: string[]
-  version: string
+  /**
+   * The version of the plugin.
+   * @example '1.0.0'
+   */
+  version?: string
   type: PluginType
   isLoaded: () => boolean
   load: (ctx: Ctx, instance: Analytics) => Promise<unknown>
