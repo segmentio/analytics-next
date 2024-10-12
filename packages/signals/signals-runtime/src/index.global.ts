4fc240ed4386476ff43fafbdff22e8f7c66a0eef
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-10-12 14:34:38 -0500

    wip

--- packages/signals/signals-runtime/src/index.global.ts
+++ packages/signals/signals-runtime/src/index.global.ts
@@ -2,6 +2,4 @@ import { SignalsRuntime } from './index'
 
 // the purpose of this is to create an artifact that can be uploaded to a CDN used in the mobile runtime, as well as the editor browser.
 // the web version can use this package directly.
-
-// @ts-ignore
-globalThis.SignalsRuntime = SignalsRuntime
+Object.assign(globalThis, { SignalsRuntime })
