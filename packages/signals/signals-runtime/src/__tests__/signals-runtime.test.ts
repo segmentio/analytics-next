32d70e6fe21a73a9e9fb8134e683c4af3c879916
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-10-12 14:57:22 -0500

    wip

--- packages/signals/signals-runtime/src/__tests__/signals-runtime.test.ts
+++ packages/signals/signals-runtime/src/__tests__/signals-runtime.test.ts
@@ -25,6 +25,20 @@ describe(SignalsRuntime, () => {
     signalsRuntime = new SignalsRuntime<Signal>([signal1, signal2, signal3])
   })
 
+  describe('meta', () => {
+    it('should be serializable / iterable', () => {
+      const copy = { ...signalsRuntime }
+      expect(copy).toEqual({
+        find: expect.any(Function),
+        signalBuffer: expect.any(Array),
+        signalCounter: expect.any(Number),
+        getNextIndex: expect.any(Function),
+        maxBufferSize: expect.any(Number),
+        add: expect.any(Function),
+      })
+    })
+  })
+
   describe('find', () => {
     it('should find following signal based on the provided function', () => {
       const result = signalsRuntime.find(signal2, 'interaction', () => true)
