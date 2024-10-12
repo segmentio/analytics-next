da1174358bb1e9a52bca8bf292e361b5f9df6216
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-10-12 16:26:45 -0500

    wip

--- packages/signals/signals-runtime/src/shared/shared-types.ts
+++ packages/signals/signals-runtime/src/shared/shared-types.ts
@@ -8,11 +8,29 @@ export type SignalOfType<
 > = AllSignals & { type: SignalType }
 
 export interface ISignalsRuntime<Signal extends BaseSignal> {
+  /**
+   * Finds a signal of a specific type from a given signal.
+   *
+   * @template SignalType - The type of the signal to find.
+   * @param fromSignal - The signal to search from.
+   * @param signalType - The type of the signal to find.
+   * @param predicate - Optional predicate function to filter the signals.
+   * @returns The found signal of the specified type, or undefined if not found.
+   */
   find: <SignalType extends Signal['type']>(
     fromSignal: Signal,
     signalType: SignalType,
     predicate?: (signal: SignalOfType<Signal, SignalType>) => boolean
   ) => SignalOfType<Signal, SignalType> | undefined
+
+  /**
+   * Filters signals of a specific type from a given signal.
+   * @template SignalType - The type of the signals to filter.
+   * @param fromSignal - The signal to search from.
+   * @param signalType - The type of the signals to filter.
+   * @param predicate - Optional predicate function to filter the signals.
+   * @returns An array of signals of the specified type.
+   */
   filter: <SignalType extends Signal['type']>(
     fromSignal: Signal,
     signalType: SignalType,
