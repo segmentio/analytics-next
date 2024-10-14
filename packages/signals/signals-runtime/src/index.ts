facef1c60bd001962e5e2a98305564cc4aba431c
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-10-14 00:08:19 -0500

    wip

--- packages/signals/signals-runtime/src/index.ts
+++ packages/signals/signals-runtime/src/index.ts
@@ -10,6 +10,10 @@ export * as WebRuntimeConstants from './web/web-constants'
 export * as Mobile from './mobile/mobile-signals-types'
 export * as MobileRuntimeConstants from './mobile/mobile-constants'
 
-// @ts-ignore - this is hacky AF
-import { getWebRuntimeString as getWebRuntimeStringUntyped } from '../../dist/global/get-runtime-string.web'
-export const getWebRuntimeString: () => string = getWebRuntimeStringUntyped
+// @ts-ignore - hacky AF
+import { getWebRuntimeString as getWebStr } from '../../dist/global/get-runtime-string.web'
+export const getWebRuntimeString: () => string = getWebStr
+
+// @ts-ignore - hacky AF
+import { getMobileRuntimeString as getMobStr } from '../../dist/global/get-runtime-string.mobile'
+export const getMobileRuntimeString: () => string = getMobStr
