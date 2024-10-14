c48c228c9b03f812be937ec6837bc1e3c3d3221c
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-10-13 21:33:58 -0500

    wip

--- packages/signals/signals-runtime/README.md
+++ packages/signals/signals-runtime/README.md
@@ -6,5 +6,5 @@ Encapsults Signals runtime functionality, in order to share logic between the si
 | Generated File(s) | Description | 
 |--------|-------------|
 | `/dist/esm/index.js` | For `@segment/analytics-signals` to consume | 
-| `/dist/global/index.mobile.js, `/dist/global/index.web.js`  |  Exposes `globalThis.SignalsRuntime`, either through the script tag or in the mobile JS engine. | 
+| `/dist/global/index.mobile.js, `/dist/global/index.web.js`  |  Exposes `globalThis.SignalsRuntime` and constants, either through the script tag or in the mobile JS engine. Meant to be uploaded to a CDN and shared in each SDK | 
 | `/editor/mobile-editor.d.ts`, `/editor/web-editor.d.ts` | Type definitions for monaco editor on app.segment.com |
--- packages/signals/signals-runtime/build-signals-runtime-global.js
+++ packages/signals/signals-runtime/build-signals-runtime-global.js
@@ -1,27 +1,39 @@
 const esbuild = require('esbuild')
 
+const pkgJSON = require('./package.json')
+const getBanner = (entryPoint) => {
+  const content = [
+    `// Generated in: ${pkgJSON.name}@${pkgJSON.version}`,
+    `// Entry point: ${entryPoint}`,
+  ].join('\n')
+  return content
+}
+
 /**
  * @param {string} type - The type of build, either 'mobile' or 'web'.
  */
 async function build(type) {
+  const entryPoint = `./src/${type}/index.global.ts`
   const outfileMinified = `./dist/global/index.${type}.min.js`
   const outfileUnminified = `./dist/global/index.${type}.js`
   try {
     // Build minified version
     await esbuild.build({
-      entryPoints: [`./src/${type}/index.global.ts`],
+      entryPoints: [entryPoint],
       outfile: outfileMinified,
       bundle: true,
       minify: true,
+      banner: { js: getBanner(entryPoint) },
     })
     console.log(`wrote: ${outfileMinified}`)
 
     // Build unminified version
     await esbuild.build({
-      entryPoints: [`./src/${type}/index.global.ts`],
+      entryPoints: [entryPoint],
       outfile: outfileUnminified,
       bundle: true,
       minify: false,
+      banner: { js: getBanner(entryPoint) },
     })
     console.log(`wrote: ${outfileUnminified}`)
   } catch (err) {
