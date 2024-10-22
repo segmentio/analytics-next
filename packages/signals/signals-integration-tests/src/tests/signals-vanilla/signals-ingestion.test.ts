e238b8b5f66b19f844f0c018296f0fe84cb8f70b
Daniel Jackins <djackins@twilio.com>
2024-10-22 10:40:36 -0600

    add ingestion integration tests

---
+++ packages/signals/signals-integration-tests/src/tests/signals-vanilla/signals-ingestion.test.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test'
+import { waitForCondition } from '../../helpers/playwright-utils'
+import { IndexPage } from './index-page'
+
+const indexPage = new IndexPage()
+
+const basicEdgeFn = `const processSignal = (signal) => {}`
+
+test('ingestion not enabled -> will not send the signal', async ({ page }) => {
+  await indexPage.loadAndWait(page, basicEdgeFn, {
+    enableSignalsIngestion: false,
+  })
+
+  await indexPage.fillNameInput('John Doe')
+  const promise = await indexPage.waitForSignalsApiFlush()
+
+  await expect(promise).rejects.toThrow()
+})
+
+test('ingestion enabled -> will send the signal', async ({ page }) => {
+  await indexPage.loadAndWait(page, basicEdgeFn, {
+    enableSignalsIngestion: true,
+  })
+
+  await Promise.all([
+    indexPage.fillNameInput('John Doe'),
+    indexPage.waitForSignalsApiFlush(),
+  ])
+
+  await waitForCondition(
+    () => indexPage.signalsAPI.getEvents('interaction').length > 0,
+    { errorMessage: 'No interaction signals found' }
+  )
+
+  const interactionSignals = indexPage.signalsAPI.getEvents('interaction')
+  expect(interactionSignals.length > 0)
+})
