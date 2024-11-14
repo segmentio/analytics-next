f8f033a8056af9755bcc2ed27fe96ae85fb09e23
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2024-11-14 17:55:11 -0500

    More codecov fixes

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Clear build artifacts and build cache
 
-find . \( -name ".turbo" -o -name "dist" -o -name ".next" -o -name "tsconfig.tsbuildinfo" \) ! -path "*/node_modules/*" -print0 | xargs -0 rm -rf
+find . \( -name ".turbo" -o -name "dist" -o -name ".next" -o -name "tsconfig.tsbuildinfo" -o -name "coverage" \) ! -path "*/node_modules/*" -print0 | xargs -0 rm -rf
 rm -rf node_modules/.cache
 
 echo "Build files and cache deleted."
