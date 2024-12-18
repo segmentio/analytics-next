53a06e6d7b6883ccce120a59a3335ddcaf732ac2
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-12-18 16:05:55 -0600

    wip

--- packages/browser/src/plugins/segmentio/shared-dispatcher.ts
+++ packages/browser/src/plugins/segmentio/shared-dispatcher.ts
@@ -18,36 +18,37 @@ export type AdditionalHeaders =
   | Record<string, string>
   | (() => Record<string, string>)
 
-/**
- * Priority of the request.
- * chrome only
- * @default 'auto'
- */
 export type FetchPriority = 'high' | 'low' | 'auto'
 
-export type BatchingDispatchConfig = {
-  size?: number
-  timeout?: number
-  maxRetries?: number
-  additionalHeaders?: AdditionalHeaders
-  /**
-   * This is useful for ensuring that events are sent even if the user navigates away from the page.
-   * @default false IF the page is being unloaded, true otherwise
-   */
-  keepalive?: boolean
-  fetchPriority?: FetchPriority
-}
-
-export type StandardDispatcherConfig = {
+interface DispatchConfig {
   /**
    * This is useful for ensuring that an event is sent even if the user navigates away from the page.
    * However, it may increase the likelihood of events being lost, as there is a 64kb limit for all fetch requests with keepalive (which is why it's disabled by default).
    * @default false
    */
   keepalive?: boolean
+  /**
+   * Additional headers to be sent with the request.
+   * Default is `Content-Type: text/plain`. This can be overridden.
+   * If a function is provided, it will be called before each request.
+   * @example { 'Content-Type': 'application/json' } or () => { 'Content-Type': 'application/json' }
+   */
   additionalHeaders?: AdditionalHeaders
+  /**
+   * Priority of the request.
+   * chrome only
+   * @default 'auto'
+   */
   fetchPriority?: FetchPriority
 }
+
+export interface BatchingDispatchConfig extends DispatchConfig {
+  size?: number
+  timeout?: number
+  maxRetries?: number
+}
+
+export interface StandardDispatcherConfig extends DispatchConfig {}
 
 export type DeliveryStrategy =
   | {
