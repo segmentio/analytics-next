3950e40b0114ae3820d9e7d7463992eb8b295b07
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-01-31 15:16:05 -0600

    wip

--- packages/signals/signals-integration-tests/src/helpers/log-console.ts
+++ packages/signals/signals-integration-tests/src/helpers/log-console.ts
@@ -2,7 +2,13 @@ import { Page } from '@playwright/test'
 
 export const logConsole = (page: Page) => {
   page.on('console', (msg) => {
-    console.log(`console.${msg.type()}:`, msg.text())
+    const CONSOLE_BLACKLIST = ['Bad Request']
+    const text = msg.text()
+    // do not print errors that are expected -- e.g. network errors set up to test the unhappy path.
+    if (CONSOLE_BLACKLIST.some((str) => text.includes(str))) {
+      return
+    }
+    console.log(`console.${msg.type()}:`, text)
   })
   page.on('pageerror', (error) => {
     console.error('Page error:', error)
