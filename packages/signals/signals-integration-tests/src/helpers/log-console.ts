83e6e1bfd50ce5d3029071841b2a3723d4e28310
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-01-30 20:54:54 -0600

    wip

--- packages/signals/signals-integration-tests/src/helpers/log-console.ts
+++ packages/signals/signals-integration-tests/src/helpers/log-console.ts
@@ -2,12 +2,7 @@ import { Page } from '@playwright/test'
 
 export const logConsole = (page: Page) => {
   page.on('console', (msg) => {
-    const text = msg.text()
-    if (text.includes('NAME_NOT_RESOLVED')) {
-      // dns error spam started showing up -- either a chrome change or a vpn issue
-      return
-    }
-    console.log(`console.${msg.type()}:`, text)
+    console.log(`console.${msg.type()}:`, msg.text())
   })
   page.on('pageerror', (error) => {
     console.error('Page error:', error)
