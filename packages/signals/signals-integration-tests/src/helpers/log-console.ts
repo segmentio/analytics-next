bab325fe37fad8573841bdd2bc61e84dcae1eddc
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-01-31 15:19:12 -0600

    wip

--- packages/signals/signals-integration-tests/src/helpers/log-console.ts
+++ packages/signals/signals-integration-tests/src/helpers/log-console.ts
@@ -1,11 +1,11 @@
 import { Page } from '@playwright/test'
 
 export const logConsole = (page: Page) => {
-  const CONSOLE_BLACKLIST = ['Bad Request']
   page.on('console', (msg) => {
     const text = msg.text()
     // keep stdout clean, e.g. by not printing intentional errors
-    if (CONSOLE_BLACKLIST.some((str) => text.includes(str))) {
+    const blacklist = ['Bad Request']
+    if (blacklist.some((str) => text.includes(str))) {
       return
     }
     console.log(`console.${msg.type()}:`, text)
