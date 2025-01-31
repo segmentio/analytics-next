90b27f6f4ffcb26f9f4694bf7b3c4809274526e6
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-01-31 16:17:16 -0600

    add integration tests

---
+++ packages/signals/signals-integration-tests/src/tests/signals-vanilla/middleware.test.ts
@@ -0,0 +1,61 @@
+import { test, expect } from '@playwright/test'
+import { IndexPage } from './index-page'
+
+const basicEdgeFn = `const processSignal = (signal) => {}`
+
+let indexPage: IndexPage
+
+test('middleware', async ({ page }) => {
+  indexPage = await new IndexPage().load(
+    page,
+    basicEdgeFn,
+    {},
+    {
+      skipSignalsPluginInit: true,
+    }
+  )
+
+  await page.evaluate(
+    ({ settings }) => {
+      window.signalsPlugin = new window.SignalsPlugin({
+        middleware: [
+          {
+            load() {
+              return undefined
+            },
+            process: function (signal) {
+              // @ts-ignore
+              signal.data['middleware'] = 'test'
+              return signal
+            },
+          },
+        ],
+        ...settings,
+      })
+      window.analytics.load({
+        writeKey: '<SOME_WRITE_KEY>',
+        plugins: [window.signalsPlugin],
+      })
+    },
+    {
+      settings: {
+        ...indexPage.defaultSignalsPluginTestSettings,
+        flushAt: 1,
+      },
+    }
+  )
+
+  /**
+   * Make an analytics.page() call, see that the middleware can modify the event
+   */
+  await Promise.all([
+    indexPage.makeAnalyticsPageCall(),
+    indexPage.waitForSignalsApiFlush(),
+  ])
+
+  const instrumentationEvents =
+    indexPage.signalsAPI.getEvents('instrumentation')
+  expect(instrumentationEvents).toHaveLength(1)
+  const ev = instrumentationEvents[0]
+  expect(ev.properties!.data['middleware']).toEqual('test')
+})
