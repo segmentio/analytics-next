3a9e02a2984deff83311557a79151a2fb87ae8e6
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-03-03 12:44:37 -0600

    wip

--- scripts/purge-cdn-cache.js
+++ scripts/purge-cdn-cache.js
@@ -6,6 +6,7 @@ const path = require('node:path')
  * Will abort the API unless the current commit has a tag with the package name on it.
  */
 const purgeJsDelivrCache = async (packageName, relativePath) => {
+  console.log(`\n\n`)
   const gitRoot = path.resolve(__dirname, '..')
   const fullPath = path.join(gitRoot, relativePath)
 
@@ -14,22 +15,27 @@ const purgeJsDelivrCache = async (packageName, relativePath) => {
     process.exit(1)
   }
 
+  // Only run this script if the given package has been published
   // Check if the current git HEAD has a tag containing the package name
-  try {
-    const tags = execSync('git tag --contains HEAD', {
-      cwd: gitRoot,
-    }).toString()
+  // This is a bit odd versus just having the cache purge on a prepublish hooks, but publish hooks like post/prepublish had been flaky on CI with changesets when I was setting this up.
+  if (process.env.CI) {
+    console.log('Searching for matching tag for package...')
+    try {
+      const tags = execSync('git tag --contains HEAD', {
+        cwd: gitRoot,
+      }).toString()
 
-    const hasMatchingTag = tags.includes(packageName)
-    if (!hasMatchingTag) {
-      console.log(
-        `No tags containing the package name "${packageName}" found on the current git HEAD. Aborting cache purge.`
-      )
-      process.exit(0)
+      const hasMatchingTag = tags.includes(packageName)
+      if (!hasMatchingTag) {
+        console.log(
+          `No tags containing the package name "${packageName}" found on the current git HEAD. Aborting cache purge.`
+        )
+        process.exit(0)
+      }
+    } catch (error) {
+      console.error(`Failed to check git tags: ${error.message}`)
+      process.exit(1)
     }
-  } catch (error) {
-    console.error(`Failed to check git tags: ${error.message}`)
-    process.exit(1)
   }
 
   const files = fs.readdirSync(fullPath)
@@ -39,19 +45,19 @@ const purgeJsDelivrCache = async (packageName, relativePath) => {
   )
   for (const file of files) {
     const filePath = path.join(relativePath, file)
+    console.log(`Purging cache: ${file}...`)
     const url = `https://purge.jsdelivr.net/npm/${packageName}/${filePath}`
 
     try {
       const response = await fetch(url)
-      if (response.ok) {
-        console.log(`Purged: ${url} - Status: ${response.status}`)
-      } else {
+      if (!response.ok) {
         console.error(`Failed to purge: ${url} - Status: ${response.status}`)
       }
     } catch (error) {
       console.error(`Failed to purge: ${url} - Error: ${error.message}`)
     }
   }
+  console.log(`\nPurge of ${packageName} finished.`)
 }
 
 const [packageName, relativePath] = process.argv.slice(2)
