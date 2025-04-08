41c59198ae85e0e53e30e5d4462bd22a90ff56d8
stephen-pope-customer-io <112018853+stephen-pope-customer-io@users.noreply.github.com>
2025-04-08 16:16:31 -0400

    Add Start of a Mise Config (#50)
    * Add Start of a Mise Config
    
    It's due
    
    * code review feedback
    
    * Prevent teardown error in test
    
    * Fix cache step
    
    From https://github.com/actions/setup-node/blob/main/docs/advanced-usage.md#caching-packages-data
    
    To fix this:
    
    Post job cleanup.
    Error: Path Validation Error: Path(s) specified in the action for caching do(es) not exist, hence no cache is being saved.

--- packages/browser/src/lib/__tests__/parse-cdn.test.ts
+++ packages/browser/src/lib/__tests__/parse-cdn.test.ts
@@ -1,7 +1,7 @@
 import { JSDOM, VirtualConsole } from 'jsdom'
 import { getCDN } from '../parse-cdn'
 
-function withTag(tag: string) {
+async function withTag(tag: string) {
   const html = `
     <!DOCTYPE html>
       <head>
@@ -33,6 +33,10 @@ function withTag(tag: string) {
   documentSpy.mockImplementation(
     () => jsd.window.document as unknown as Document
   )
+
+  await new Promise((resolve) => {
+    jsd.window.addEventListener('load', () => resolve(undefined));
+  });
 }
 
 beforeEach(async () => {
@@ -40,15 +44,15 @@ beforeEach(async () => {
   jest.resetAllMocks()
 })
 
-it('detects the existing CDN', () => {
-  withTag(`
+it('detects the existing CDN', async () => {
+  await withTag(`
     <script src="https://cdp.customer.io/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZvCF6czfBcfzNO6/analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"https://cdp.customer.io"`)
 })
 
-it('should return the overridden cdn if window.analytics._cdn is mutated', () => {
-  withTag(`
+it('should return the overridden cdn if window.analytics._cdn is mutated', async () => {
+  await withTag(`
   <script src="https://cdp.customer.io/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZvCF6czfBcfzNO6/analytics.min.js" />
   `)
   // @ts-ignore
@@ -58,44 +62,44 @@ it('should return the overridden cdn if window.analytics._cdn is mutated', () =>
   expect(getCDN()).toMatchInlineSnapshot(`"http://foo.cdn.com"`)
 })
 
-it('if analytics is not loaded yet, should still return cdn', () => {
+it('if analytics is not loaded yet, should still return cdn', async () => {
   // is this an impossible state?
   // @ts-ignore
   window.analytics = undefined as any
-  withTag(`
+  await withTag(`
   <script src="https://cdp.customer.io/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZvCF6czfBcfzNO6/analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"https://cdp.customer.io"`)
 })
 
-it('detects custom cdns that match in domain instrumentation patterns', () => {
-  withTag(`
+it('detects custom cdns that match in domain instrumentation patterns', async () => {
+  await withTag(`
     <script src="https://my.cdn.domain/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZvCF6czfBcfzNO6/analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"https://my.cdn.domain"`)
 })
 
-it('detects custom cdns if the write key has : in it', () => {
-  withTag(`
+it('detects custom cdns if the write key has : in it', async () => {
+  await withTag(`
     <script src="https://my.cdn.domain/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZv:CF6czfBcfzNO6/analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"https://my.cdn.domain"`)
 })
 
-it('falls back if CDN is used as a proxy', () => {
-  withTag(`
+it('falls back if CDN is used as a proxy', async () => {
+  await withTag(`
     <script src="https://my.cdn.proxy/custom-analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"https://cdp.customer.io"`)
 })
 
-it('falls back if the script is not at all present on the page', () => {
-  withTag('')
+it('falls back if the script is not at all present on the page', async () => {
+  await withTag('')
   expect(getCDN()).toMatchInlineSnapshot(`"https://cdp.customer.io"`)
 })
 
-it('detects localhost with port', () => {
-  withTag(`
+it('detects localhost with port', async () => {
+  await withTag(`
     <script src="http://localhost:3000/v1/analytics-js/snippet/gA5MBlJXrtZaB5sMMZv/analytics.min.js" />
   `)
   expect(getCDN()).toMatchInlineSnapshot(`"http://localhost:3000"`)
