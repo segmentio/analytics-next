6a7638258958302b14c4a1608180282363345823
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-04-09 13:23:17 -0500

    update build

--- packages/config/src/tsup/config.js
+++ packages/config/src/tsup/config.js
@@ -2,11 +2,18 @@ const { defineConfig } = require('tsup')
 
 module.exports = defineConfig(() => ({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm', 'cjs'], // Output both ESM and CJS formats
   dts: {
     resolve: true,
     entry: 'src/index.ts',
   },
-  outDir: 'dist',
   clean: true,
+  esbuildOptions(options, context) {
+    if (context.format === 'esm') {
+      options.outdir = 'dist/esm' // Output ESM files to dist/esm
+      options.outExtension = { '.js': '.mjs' } // Use .mjs for ESM files
+    } else if (context.format === 'cjs') {
+      options.outdir = 'dist/cjs' // Output CJS files to dist/cjs
+    }
+  },
 }))
