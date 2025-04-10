21a007d2550979981308f8c94e912c18bd5ab078
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-04-10 13:45:46 -0500

    add differences

--- packages/signals/signals/src/core/signal-generators/dom-gen/dom-gen.ts
+++ packages/signals/signals/src/core/signal-generators/dom-gen/dom-gen.ts
@@ -1,3 +1,4 @@
+import { ChangedProperties } from '@segment/analytics-signals-runtime'
 import { URLChangeObservable } from '../../../lib/detect-url-change'
 import {
   createInteractionSignal,
@@ -79,6 +80,27 @@ export const shouldIgnoreElement = (el: HTMLElement): boolean => {
   return false
 }
 
+function getObjectDifferences<T extends object>(obj1: T, obj2: T): string[] {
+  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)])
+  return Array.from(keys).filter(
+    (key) => obj1[key as keyof T] !== obj2[key as keyof T]
+  )
+}
+
+function getURLDifferences(current: URL, previous: URL): ChangedProperties[] {
+  const difference = getObjectDifferences(current, previous)
+  const changed: ChangedProperties[] = []
+  for (const k of difference) {
+    if (k === 'pathname') {
+      changed.push('path')
+    }
+    if (['search', 'hash'].includes(k)) {
+      changed.push(k as ChangedProperties)
+    }
+  }
+  return changed
+}
+
 export class OnNavigationEventGenerator implements SignalGenerator {
   id = 'navigation'
 
@@ -93,11 +115,12 @@ export class OnNavigationEventGenerator implements SignalGenerator {
 
     // emit a navigation signal whenever the URL has changed
     const urlChange = new URLChangeObservable()
-    urlChange.subscribe((prevUrl) =>
+    urlChange.subscribe(({ previous, current }) =>
       emitter.emit(
         createNavigationSignal({
           action: 'urlChange',
-          prevUrl,
+          prevUrl: previous.href,
+          changedProperties: getURLDifferences(current, previous),
           ...this.createCommonFields(),
         })
       )
--- packages/signals/signals/src/lib/detect-url-change/index.ts
+++ packages/signals/signals/src/lib/detect-url-change/index.ts
@@ -1,11 +1,23 @@
 import { Emitter } from '@segment/analytics-generic-utils'
 
+const safeParseURL = (url: string): URL | null => {
+  try {
+    return new URL(url)
+  } catch (e) {
+    return null
+  }
+}
+
+type ChangeData = {
+  current: URL
+  previous: URL
+}
 // This seems hacky, but if using react router (or other SPAs), popstate will not always fire on navigation
 // Otherwise, we could use popstate / hashchange events
 export class URLChangeObservable {
-  private emitter = new Emitter()
+  private emitter = new Emitter<{ change: [ChangeData] }>()
   private pollInterval = 500
-  urlChanged?: (url: string) => void
+  private urlChanged?: (data: ChangeData) => void
   constructor() {
     this.pollURLChange()
   }
@@ -15,13 +27,18 @@ export class URLChangeObservable {
     setInterval(() => {
       const currentUrl = window.location.href
       if (currentUrl != prevUrl) {
-        this.emitter.emit('change', prevUrl)
+        const current = safeParseURL(currentUrl)!
+        const prev = safeParseURL(prevUrl)!
+        this.emitter.emit('change', {
+          current,
+          previous: prev,
+        })
         prevUrl = currentUrl
       }
     }, this.pollInterval)
   }
 
-  subscribe(urlChanged: (newURL: string) => void) {
+  subscribe(urlChanged: (data: ChangeData) => void) {
     this.urlChanged = urlChanged
     this.emitter.on('change', this.urlChanged)
   }
