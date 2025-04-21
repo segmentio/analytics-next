64136f7523cd8cc7de8af3b56570e4d152aa0e0e
Neelkanth Kaushik <nkaushik@segment.com>
2025-04-21 14:34:57 +0530

    Removed wdio.conf.local.ts

--- packages/consent/consent-tools-integration-tests/playwright.config.ts
+++ packages/consent/consent-tools-integration-tests/playwright.config.ts
@@ -4,7 +4,7 @@ const PORT = 4567
 const config: PlaywrightTestConfig = {
   testDir: './src/tests',
   timeout: 30000,
-  retries: 2,
+  retries: 1,
   webServer: {
     command: 'yarn http-server public -p 4567',
     port: 4567,
@@ -14,6 +14,10 @@ const config: PlaywrightTestConfig = {
     headless: true,
     viewport: { width: 1280, height: 720 },
     baseURL: `http://localhost:${PORT}`,
+    video: {
+      mode: 'off',
+      size: { width: 640, height: 480 },
+    },
   },
 }
 
