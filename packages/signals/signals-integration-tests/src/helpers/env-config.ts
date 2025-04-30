83ff15d4b8c81b71563b00c561f83854b734aff5
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-04-30 13:38:24 -0500

    add env-config.ts

--- packages/signals/signals-integration-tests/src/helpers/env-config.ts
+++ packages/signals/signals-integration-tests/src/helpers/env-config.ts
@@ -1,9 +1,9 @@
-import { SignalsSettingsConfig } from '@segment/analytics-signals/dist/types/core/signals'
+import { SignalsPluginSettingsConfig } from '@segment/analytics-signals'
 
 // This is for testing with the global sandbox strategy with an npm script, that executes processSignal in the global scope
 // If we change this to be the default, this can be rejiggered
 const SANDBOX_STRATEGY = (process.env.SANDBOX_STRATEGY ??
-  'iframe') as SignalsSettingsConfig['sandboxStrategy']
+  'iframe') as SignalsPluginSettingsConfig['sandboxStrategy']
 
 export const envConfig = {
   SANDBOX_STRATEGY,
