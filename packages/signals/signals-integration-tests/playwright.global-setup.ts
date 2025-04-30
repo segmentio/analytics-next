36b1f371121df28e5ec378a8eff3535fabed0294
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-04-30 13:36:16 -0500

    update t3ests

--- packages/signals/signals-integration-tests/playwright.global-setup.ts
+++ packages/signals/signals-integration-tests/playwright.global-setup.ts
@@ -5,6 +5,9 @@ import { envConfig } from './src/helpers/env-config'
 export default function globalSetup(_cfg: FullConfig) {
   console.log(`Executing playwright.global-setup.ts...\n`)
   console.log(`Using envConfig: ${JSON.stringify(envConfig, undefined, 2)}\n`)
-  execSync('yarn build', { stdio: 'inherit' })
-  console.log('Finished global setup.\n')
+  if (process.env.SKIP_BUILD !== 'true') {
+    console.log(`Executing yarn build:\n`)
+    execSync('yarn build', { stdio: 'inherit' })
+  }
+  console.log('Finished global setup. Should start running tests.\n')
 }
