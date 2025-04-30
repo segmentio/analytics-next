6c266aadbb7a3ff17a3242d821c99edd17d5a039
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-04-30 17:27:04 -0500

    wip

--- packages/signals/signals/src/core/middleware/network-signals-filter/network-signals-filter.ts
+++ packages/signals/signals/src/core/middleware/network-signals-filter/network-signals-filter.ts
@@ -85,7 +85,13 @@ export class NetworkSignalsFilterList {
     this.disallowedDefaults = new NetworkFilterListItem([
       'api.segment.io',
       'signals.segment.io',
-      'cdn.segment.com',
+      'cdn.segment',
+      'webpack',
+      'hot-update',
+      'hmr',
+      'browser-sync',
+      'livereload',
+      'esbuild',
     ])
   }
 
