d773b6bd164d700a28fcc49c6a46f95308445fa9
Seth Silesky <5115498+silesky@users.noreply.github.com>
2025-05-09 15:46:55 -0500

    remove unneeded globals

--- packages/consent/consent-tools-integration-tests/src/page-bundles/onetrust/index.ts
+++ packages/consent/consent-tools-integration-tests/src/page-bundles/onetrust/index.ts
@@ -1,13 +1,6 @@
 import { AnalyticsBrowser } from '@segment/analytics-next'
 import { withOneTrust } from '@segment/analytics-consent-wrapper-onetrust'
 
-declare global {
-  interface Window {
-    withOneTrust: typeof import('@segment/analytics-consent-wrapper-onetrust').withOneTrust
-  }
-}
-window.withOneTrust = withOneTrust
-
 const analytics = new AnalyticsBrowser()
 
 withOneTrust(analytics).load(
